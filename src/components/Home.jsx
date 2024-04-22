0dae1ef7138e6cbe99bfdb5a6a3ef62ce0e815ce
Abubakar <134952628+Abubakar-creates@users.noreply.github.com>
2024-04-22 14:25:34 +0000

    resume update

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -21,7 +21,7 @@ const Home = () => {
         </div>
         <div className="flex flex-col justify-center h-full">
           <h2 className="text-4xl sm:text-7xl font-bold text-white">
-            I'm a React & React Native Developer
+            I'm a MERN Developer
           </h2>
           <p className="text-gray-500 py-4 max-w-md">
             I have 2 years of experience building websites and desgining software.
